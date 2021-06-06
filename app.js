d55c75d98560019a4e9bda4543fa79db20b274b6
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2021-06-06 22:34:57 +0200

    added home screen and campioni

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
@@ -14,16 +15,44 @@ app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname, 'about.html')); //__dirname : Ritorna la cartella del progetto
 });
 
-app.get('/sitemap', function (req, res) {
-    res.sendFile(path.join(__dirname, 'sitemap.html')); //__dirname : Ritorna la cartella del progetto
+app.get('/campioni', function (req, res) {
+    res.sendFile(path.join(__dirname, 'campioni.html')); //__dirname : Ritorna la cartella del progetto
 });
 
 app.get('/nav.html', function (req, res) {
     res.sendFile(path.join(__dirname, 'nav.html')); //__dirname : Ritorna la cartella del progetto
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/assets/nunu_animation.mp4', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/nunu_animation.mp4')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/fonts/beaufortforlol-regular.ttf', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/fonts/beaufortforlol-regular.ttf')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/fonts/beaufortforlol-italic.ttf', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/fonts/beaufortforlol-italic.ttf')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/images/akali-card.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/images/akali-card.jpg')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/images/aphelios-card.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/images/aphelios-card.jpg')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/images/garen-card.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/images/garen-card.jpg')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/assets/images/ryze-card.jpg', function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets/images/ryze-card.jpg')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.listen(5000, function () {
+    console.log('Example app listening on port 5000!');
 });
 
 
